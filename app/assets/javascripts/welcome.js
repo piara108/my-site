9522deb41c49ffb81a591d6fbc9066f952b47b72
Piara Sandhu <piarasandhu108@gmail.com>
2016-06-09 16:46:22 -0700

    Tweak jQuery

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -10,11 +10,11 @@ $(document).ready(function() {
   //   $(this).find('li').css('font-size', '1.5em');
   // });
 
-  $('.nav-items').on('mouseenter', function() {
+  $('.list-items').on('mouseenter', function() {
     $(this).find('li').css('font-size', '3em');
   });
 
-  $('.nav-items').on('mouseleave', function() {
+  $('.list-items').on('mouseleave', function() {
     $(this).find('li').css('font-size', '1.5em');
   });
 });
