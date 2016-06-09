32e99b8fb618d06cf0bd83259588d60c6eef7d5c
Piara Sandhu <piarasandhu108@gmail.com>
2016-06-09 15:51:51 -0700

    Add jQuery to site

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,8 @@
+// JS for hovering over the link
+$(document).ready(function() {
+  console.log("You are in the console right now!!");
+
+  $('.whole-page').on('mouseenter', function() {
+    $(this).find("li").css("font-size", "10em");
+  });
+});
