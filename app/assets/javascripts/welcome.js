273e8f05fae62d7dc9bad8d9f2beab3fe93d52f1
Piara Sandhu <piarasandhu108@gmail.com>
2016-06-09 16:20:55 -0700

    Tweak jQuery

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,11 +2,11 @@
 $(document).ready(function() {
   console.log("You are in the console right now!!");
 
-  $('.nav-items').on('mouseenter', function() {
+  $('ul .nav-items').on('mouseenter', function() {
     $(this).find('li').css('font-size', '2em');
   });
 
-  $('.nav-items').on('mouseleave', function() {
+  $('ul .nav-items').on('mouseleave', function() {
     $(this).find('li').css('font-size', '1.5em');
   });
 });
