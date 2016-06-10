f7e7a71d6c2a72fb9951ec9a016a159a1d6505a6
Piara Sandhu <piarasandhu108@gmail.com>
2016-06-10 00:38:35 -0700

    Tweak JS

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,16 +3,19 @@
 //
 // });
 
-$(document).on("page:change", function() {
-  $(document).trigger("ready");
+
+var ready;
+// $(document).on("page:change", function() {
+  // $(document).trigger("ready");
+ready = function() {
 
   console.log("The page has loaded");
 
   $(".links").hover(function() {
     $(this).css("padding-left", "1em");
   });
-});
+};
 
 
 // $(document).ready(ready);
-// $(document).on('page:load', ready);
+$(document).on('page:load', ready);
