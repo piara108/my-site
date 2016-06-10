21acc34b1025f3ad71face3be84982eb2388d83f
Piara Sandhu <piarasandhu108@gmail.com>
2016-06-10 00:29:24 -0700

    Tweak JS

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -8,30 +8,11 @@ $(document).on("page:change", function() {
 
   console.log("The page has loaded");
 
-  $(".link-set").on("mouseenter", function() {
-    $(".links").each(function() {
-      $(this).css("padding-left", "1em");
-    });
+  $(".links").hover(function() {
+    $(this).css("padding-left", "1em");
   });
-
-  $(".link-set").on("mouseleave", function() {
-    $(".links").each(function() {
-      $(this).css("padding-left", "0em");
-    });
-  });
-
-
-  // $(".links").each(function() {
-  //   $(this).on("mouseenter", function() {
-  //     $(this).css("padding-left", "1em");
-  //   });
-  //
-  //   $(this).on("mouseleave", function() {
-  //     $(this).css("padding-left", "0em");
-  //   });ra
-  // })
 });
 
 
-// $(document).ready(ready);
-// $(document).on('page:load', ready);
+$(document).ready(ready);
+$(document).on('page:load', ready);
