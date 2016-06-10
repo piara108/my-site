a8dabe15cc532ec7186f3e7f2d520f8c46db1194
Piara Sandhu <piarasandhu108@gmail.com>
2016-06-09 23:12:55 -0700

    Tweak jQuery

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -18,27 +18,5 @@ $(document).on("page:change", function() {
 });
 
 
-
-// var ready;
-// ready = function() {
-//   console.log("You are in the console right now!!");
-//
-//   $(".links").on("mouseenter", function() {
-//     $(this).find("li").css("margin-left", "2em");
-//   });
-//
-//   $(".links").on("mouseleave", function() {
-//     $(this).find("li").css("margin-left", "-0.2em");
-//   });
-// };
-//
 // $(document).ready(ready);
 // $(document).on('page:load', ready);
-//
-// $(".nav-items").mouseenter(function() {
-//   $(this).find("li").css("margin-left", "2em");
-// });
-//
-// $(".nav-items").mouseleave(function() {
-//   $(this).find("li").css("margin-left", "-0.05em");
-// });
