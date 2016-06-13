a4cef64338f14fae8d45e3982847da95172d1ed3
Piara Sandhu <piarasandhu108@gmail.com>
2016-06-13 15:19:30 -0700

    Add jQuery to site

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -4,18 +4,22 @@
 // });
 
 
-var ready;
-// $(document).on("page:change", function() {
-  // $(document).trigger("ready");
-ready = function() {
+// var ready;
+// // $(document).on("page:change", function() {
+//   // $(document).trigger("ready");
+// ready = function() {
+//
+//   console.log("The page has loaded");
+//
+//   $(".links").hover(function() {
+//     $(this).css("padding-left", "1em");
+//   });
+// };
+//
+//
+// $(document).ready(ready);
+// $(document).on('page:load', ready);
 
-  console.log("The page has loaded");
-
-  $(".links").hover(function() {
-    $(this).css("padding-left", "1em");
-  });
-};
-
-
-$(document).ready(ready);
-$(document).on('page:load', ready);
+$(document).ready(function() {
+  console.log("You're in the console");
+});
